7bc61179113060a1d25447d244d8edd11c207039
Muhammad Tier Sinyo C.U.S. <118105588+Stalker-moment@users.noreply.github.com>
2024-08-20 11:39:55 +0700

    Update sendDowntimeLogs.js

--- functions/sendDowntimeLogs.js
+++ functions/sendDowntimeLogs.js
@@ -2,7 +2,7 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 /**
- * Fetch logs filtered by a specific month or return all logs for the month.
+ * Fetch logs filtered by a specific month or return all logs for the current month.
  * Convert timeStart and timeEnd into string fields in HH:MM:SS format.
  * @param {String|null} filterMonth - The month to filter logs by in YYYY-MM format. If null, fetch logs for the current month.
  * @returns {Promise<Array>} - A promise that resolves to an array of logs with formatted time and date.
@@ -12,15 +12,13 @@ async function sendDowntimeLogs(filterMonth = null) {
   try {
     let downTime;
 
-    // Determine the start and end of the month based on the filterMonth
-    let startOfMonth;
-    let endOfMonth;
-
-    if (filterMonth) {
+    // Check if filterMonth is provided and is a string
+    if (filterMonth && typeof filterMonth === 'string') {
       const [year, month] = filterMonth.split("-").map(Number);
       startOfMonth = new Date(year, month - 1, 1);
       endOfMonth = new Date(year, month, 0, 23, 59, 59, 999);
     } else {
+      // Default to the current month
       const today = new Date();
       startOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
       endOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0, 23, 59, 59, 999);
