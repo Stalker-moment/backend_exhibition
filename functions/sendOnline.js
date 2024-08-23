2d2f830f5096d1354cc598c2f47b8726abfd7031
Muhammad Tier Sinyo C.U.S. <118105588+Stalker-moment@users.noreply.github.com>
2024-08-24 00:17:52 +0700

    updated

--- functions/sendOnline.js
+++ functions/sendOnline.js
@@ -10,6 +10,9 @@ async function sendOnline() {
       },
     });
 
+    //get Data from oeeConfig
+    let DataBefore = await prisma.oeeConfig.findFirst();
+
     // Check if a log entry exists
     if (!latestConfig) {
       return { message: "No found" };
@@ -21,15 +24,48 @@ async function sendOnline() {
     //get the current time
     let currentTime = new Date();
 
+    //get downtime status
+    let downtime = DataBefore.onDownTime;
+
+    //get last data from downtime
+    let downtimeData = await prisma.downTime.findFirst({
+      orderBy: {
+        id: "desc",
+      },
+    });
+
+    //get time start downtime
+    let timeStart = downtimeData.timeStart.getTime();
+
+    //compare the time now and the time start downtime
+    let timeDifference = currentTime - timeStart;
+
+    //convert time difference to format HH:MM:SS
+    let hours = Math.floor(timeDifference / 3600000);
+    let minutes = Math.floor((timeDifference % 3600000) / 60000);
+    let seconds = Math.floor((timeDifference % 60000) / 1000);
+
+    let timeDifferenceHHMMSS = `${hours}:${minutes}:${seconds}`;
+
     //get the difference between the last log and the current time
     let difference = currentTime - lastLog;
 
     //check if the difference is greater than 10 seconds
     //console.log(difference);
     if (difference > 10000) {
-      return { isOnline: false, message: "Machine Offline" };
+      return {
+        isOnline: false,
+        downtime: false,
+        time: timeDifferenceHHMMSS,
+        message: "Machine Offline",
+      };
     } else {
-      return { isOnline:true, message: "Machine Online" };
+      return {
+        isOnline: true,
+        downtime,
+        time: timeDifferenceHHMMSS,
+        message: "Machine Online",
+      };
     }
   } catch (error) {
     console.error("Error fetching latest log:", error);
