354c4c5c465afb0111dbcd4a37254b9bbc0440a6
Muhammad Tier Sinyo C.U.S. <118105588+Stalker-moment@users.noreply.github.com>
2024-08-25 15:37:47 +0700

    Update edit.js

--- controllers/users/edit.js
+++ controllers/users/edit.js
@@ -127,4 +127,139 @@ router.post("/edit", async (req, res) => {
   }
 });
 
+router.post("/edit/others", async (req, res) => {
+  const { email, password, firstName, lastName, phone, noreg } = req.body;
+  const { authorization } = req.headers;
+
+  try {
+    if (!authorization) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const token = authorization.replace("Bearer ", "");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (decoded.expired < Date.now()) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    if(decoded.role !== "admin"){
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const account = await prisma.account.findUnique({
+      where: {
+        email: email,
+      },
+      include: {
+        contact: true, // Include the contact relation
+      },
+    });
+
+    //console.log(account);
+
+    if (!account) {
+      return res.status(404).json({ error: "Account not found" });
+    }
+
+    //detect value of edited field
+    let role = account.role;
+
+    //hash password if it is edited
+    let hashedPassword = account.password;
+    if (password) {
+      hashedPassword = await bcrypt.hash(password, 10);
+    }
+
+    let firstNamed = account.contact.firstName;
+    if (firstName) {
+      firstNamed = firstName;
+    }
+
+    let lastNamed = account.contact.lastName;
+    if (lastName) {
+      lastNamed = lastName;
+    }
+
+    let phoned = account.contact.phone;
+    if (phone) {
+      phoned = phone;
+    }
+
+    let noReg = account.contact.noReg;
+    if (noreg) {
+      noReg = noreg;
+    }
+
+    const updatedAccount = await prisma.account.update({
+      where: {
+        email: decoded.email,
+      },
+      data: {
+        password: hashedPassword,
+        role: role,
+        contact: {
+          update: {
+            firstName: firstNamed,
+            lastName: lastNamed,
+            phone: phoned,
+            noReg: noReg,
+          },
+        },
+      },
+    });
+
+    if (!updatedAccount) {
+      return res.status(500).json({ error: "Failed to update account" });
+    }
+
+    if(password){
+      return res.status(201).json({ message: `Succes update account and password` });
+    } 
+
+    return res.status(200).json({ message: `Succes update account (${email}) by (${decoded.email})` });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.post("/edit/delete", async (req, res) => {
+  const { email } = req.body;
+  const { authorization } = req.headers;
+
+  try {
+    if (!authorization) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const token = authorization.replace("Bearer ", "");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (decoded.expired < Date.now()) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    if(decoded.role !== "admin"){
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    //delete account
+    const deletedAccount = await prisma.account.delete({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!deletedAccount) {
+      return res.status(500).json({ error: "Failed to delete account" });
+    }
+
+    return res.status(200).json({ message: `Succes delete account (${email}) by (${decoded.email})` });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 module.exports = router;
